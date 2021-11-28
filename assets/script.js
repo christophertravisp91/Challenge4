b193b3714b8a4f5c81c7f6d96762348dc6e50fc6
TomFoolery <christophertravisp91@gmail.com>
2021-11-28 15:55:32 -0500

    changed Java

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,73 @@
-const question = document.querySelector("#question");
-const choices = Array.from(document.querySelectorAll("#choice-text"));
-const scoreText = document.querySelector("#score");
-const timer = document.querySelector("#timer");
+let question = {
+        title: "Which are the acceptable containment elements for an Array?",
+        choice: [" {} ", " () ", " [] ", " :: "],
+        correctAnswer: 2
+    };
 
-let currentQuestion = {};
-let acceptingAnswer = true;
-let availableQuestions = {}
 
-let questions = [
-    {
-        question: "Which are the acceptable containment elements for an Array?",
-        choice1: " {} ",
-        choice2: " () ",
-        choice3: " [] ",
-        choice4: " :: ",
-        answer: []
-    },
-    {
-        question: "Which are the acceptable containment elements for an Array?",
-        choice1: " {} ",
-        choice2: " () ",
-        choice3: " [] ",
-        choice4: " :: ",
-        answer: []
-    },
-    {
-        question: "Which are the acceptable containment elements for an Array?",
-        choice1: " {} ",
-        choice2: " () ",
-        choice3: " [] ",
-        choice4: " :: ",
-        answer: []
-    },
-    {
-        question: "Which are the acceptable containment elements for an Array?",
-        choice1: " {} ",
-        choice2: " () ",
-        choice3: " [] ",
-        choice4: " :: ",
-        answer: []
-    },
-]
+function showQuestion(q) {
+    let titleDiv  = document.getElementById( "title");
+
+    titleDiv.textContent = q.title;
+
+    let ch = document.querySelectorAll(".choice")
+    
+    ch.forEach(function(element, index){
+        element.textContent = q.choice[index];
+
+        element.addEventListener("click", function(){
+            if(q.correctAnswer == index) {
+                console.log("Correct!");
+            }
+            else {
+                console.log("Wrong!");
+            }
+        });
+    });
+}
+
+showQuestion(question);
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    console.log("Clicked!");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function countdown() {
+    var timeLeft = 60;
+
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+        
+            timerEl.textContent = timeLeft + ' seconds remaining';
+        // Decrement `timeLeft` by 1
+            timeLeft--;
+        } else if (timeLeft === 1) {
+        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+            timerEl.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+        } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+            timerEl.textContent = '';
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+        // Call the `displayMessage()` function
+        showScore();
+        }
+    }, 1000);
+}
